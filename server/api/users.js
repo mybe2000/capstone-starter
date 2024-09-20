53152b4fa258733b2775d7eeb82ca5d266cbe243
Myha Kalaj <mybe2000@gmail.com>
2024-09-20 17:51:06 -0400

    initial commit

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { createUser } = require("../db");
+const jwt = require("jsonwebtoken");
 
 const { fetchUsers } = require("../db");
 
@@ -8,6 +10,39 @@ router.get("/", async (req, res, next) => {
     res.send(await fetchUsers());
   } catch (ex) {
     next(ex);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log(req.body);
+  if (!username) {
+    next({ name: "UsernameRequiredErr", message: "Please provide username" });
+    return;
+  }
+  if (!password) {
+    next({ name: "PasswordRequiredErr", message: "Please provide password" });
+    return;
+  }
+  try {
+    const result = await createUser(req.body);
+    if (result) {
+      const token = jwt.sign({ id: result.id, username }, process.env.JWT, {
+        expiresIn: "1w",
+      });
+      console.log(token);
+      res.send({
+        message: "Registration successful",
+        token,
+        user: { id: result.id, username: result.username },
+      });
+      return;
+    } else {
+      next({ name: "RegistrationErr", message: "error registering" });
+      return;
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
