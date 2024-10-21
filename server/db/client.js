cd42aaa049573dc9b223298039681b8bf71849aa
Myha Kalaj <mybe2000@gmail.com>
2024-10-21 10:26:00 -0400

    update client

--- server/db/client.js
+++ server/db/client.js
@@ -2,13 +2,20 @@ const pg = require("pg");
 // const client = new pg.Client(
 //   process.env.DATABASE_URL || "postgres://localhost:5432/fsa_app_db"
 // );
+// const client = new pg.Client({
+//   connectionString:
+//     process.env.DATABASE_URL || "postgres://localhost:5432/fsa_app_db",
+//   ssl:
+//     process.env.NODE_ENV === "production"
+//       ? { rejectUnauthorized: false }
+//       : false,
+// });
 const client = new pg.Client({
-  connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:5432/fsa_app_db",
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PWD,
+  user: process.env.USER,
 });
 
 module.exports = { client };
